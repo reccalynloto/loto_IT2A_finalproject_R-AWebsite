10bbf8880ee6e0890fbd80d72c9701a0edfa9daa
reccalyn <reccalynloto@gmail.com>
2023-05-28 14:09:48 +0800

    php contact added

---
+++ contact.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.rtl.min.css" integrity="sha384-dc2NSrAXbAkjrdm9IYrX10fQq9SDG6Vjz7nQVKdKcJl3pC+k37e7qJR5MVSCS+wR" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+
+    <title>R&A Website</title>
+  </head>
+  <body>
+
+  <!---header--->
+
+      <div class="wrapper">
+        <div class="header">
+          <div>
+            <img src="images/logo.png">
+          </div>
+          <ul>
+            <li><a href="about.php">ABOUT US</a></li>
+            <li><a href="contact.php" class="active">CONTACT US</a></li>
+            <li><a href="resto.php">RESTAURANT</a></li>
+            <li><a href="hotel.php">HOTEL</a></li>
+            <li><a href="index.php">HOME</a></li>
+          </ul>
+        </div>
+      </div>
+
+
+    <!---contact us page--->
+   
+    <div class="contact-form">
+      <h2>Contact Us</h2>
+      <p> For your comments and suggestions. Send us a message and we'll respond as soon as possible</p>
+      <form class="contact" action="" method="post">
+        <input type="text" name="name" class="text-box" placeholder="Your Full Name" required>
+        <input type="email" name="email" class="text-box" placeholder="Your Email" required>
+        <textarea name="message" rows="4" placeholder="Your Message" required></textarea>
+        <input type="submit" name="submit" class="send-btn" value="Send">
+      </form>
+    </div>
+
+
+
+    <div class="footer">
+      <p> Copyright by R&A Hotel and Restaurant 2023<br> &copy; All rights reserved </p>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-facebook"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="fab instagram"><i class="bi bi-instagram"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-twitter"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-youtube"></i></a>
+    </div>  
+
+  </body>
+</html>
