38d035e51c6e325c38abd91fff468a03040a5cbb
reccalyn <reccalynloto@gmail.com>
2023-05-28 14:20:09 +0800

    php resto added

---
+++ resto.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.rtl.min.css" integrity="sha384-dc2NSrAXbAkjrdm9IYrX10fQq9SDG6Vjz7nQVKdKcJl3pC+k37e7qJR5MVSCS+wR" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+
+    <title>R&A Website</title>
+  </head>
+  <body>
+
+  <!---header--->
+
+      <div class="wrapper">
+        <div class="header">
+          <div>
+            <img src="images/logo.png">
+          </div>
+          <ul>
+            <li><a href="about.php">ABOUT US</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+            <li><a href="resto.php" class="active">RESTAURANT</a></li>
+            <li><a href="hotel.php">HOTEL</a></li>
+            <li><a href="index.php">HOME</a></li>
+          </ul>
+        </div>
+      </div>
+
+
+    <!---resto page--->
+    <div class="resto-page">
+        <h2>The R&A RESTAURANT</h2>
+        <p>"Try our best seller and savory dishes that you and your family would enjoy together with your loveones"</p>
+      </div>
+
+    <section class="menus">
+        <div class="menu">
+          <img src="images/menu/appetizer.jpg">
+          <h2>Click the button for your reservation</h2>
+          <a class="btn custom-btn" href= "appetizer.php">Appetizer</a>
+        </div>
+        <div class="menu">
+          <img src="images/menu/lunch.jpg">
+          <h2>Click the button for your reservation</h2>
+          <a class="btn custom-btn" href= "lunch.php">Lunch</a>
+        </div>
+        <div class="menu">
+          <img src="images/menu/dinner.jpeg">
+          <h2>Click the button for your reservation</h2>
+          <a class="btn custom-btn" href= "dinner.php">Dinner</a>
+        </div>
+        <div class="menu">
+          <img src="images/menu/desserts.jpeg">
+          <h2>Click the button for your reservation</h2>
+          <a class="btn custom-btn" href= "desserts.php">Desserts</a>
+        </div>
+        <div class="menu">
+          <img src="images/menu/snacks.jpeg">
+          <h2>Click the button for your reservation</h2>
+          <a class="btn custom-btn" href= "snacks.php">Snacks</a>
+        </div>
+        <div class="menu">
+          <img src="images/menu/drinks.jpeg">
+          <h2>Click the button for your reservation</h2>
+          <a class="btn custom-btn" href= "Drinks.php">Drinks</a>
+        </div>
+        
+      </section>
+
+
+
+
+
+
+    <div class="footer">
+      <p> Copyright by R&A Hotel and Restaurant 2023<br> &copy; All rights reserved </p>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-facebook"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="fab instagram"><i class="bi bi-instagram"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-twitter"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-youtube"></i></a>
+    </div>  
+
+
+
+  </body>
+</html>
