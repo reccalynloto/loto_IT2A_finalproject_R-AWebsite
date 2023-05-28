96b529dfa1aa2811385248131a33bcd0bb4ea62d
reccalyn <reccalynloto@gmail.com>
2023-05-28 14:07:42 +0800

    php about added

---
+++ about.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.rtl.min.css" integrity="sha384-dc2NSrAXbAkjrdm9IYrX10fQq9SDG6Vjz7nQVKdKcJl3pC+k37e7qJR5MVSCS+wR" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+
+    <title>R&A Website</title>
+  </head>
+  <body>
+
+  <!---header--->
+
+      <div class="wrapper">
+        <div class="header">
+          <div>
+            <img src="images/logo.png">
+          </div>
+          <ul>
+            <li><a href="about.php" class="active">ABOUT US</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+            <li><a href="resto.php">RESTAURANT</a></li>
+            <li><a href="hotel.php">HOTEL</a></li>
+            <li><a href="index.php">HOME</a></li>
+          </ul>
+        </div>
+      </div>
+
+
+    <!---about us page--->
+
+    <div class="about-page">
+        <h2>R&A HOTEL AND RESTAURANT</h2>
+        <h3>About Us</h3>
+        <p> R&A Hotel and Restaurant was built on the year 2023 and was formed by Reccalyn Loto and Arah Dianne Sadiwa<br>.The purpose is to give the satisfaction of every customer at a very affordable price<br>.Anyone can visit us anytime and you can also bring your family because we have different types of rooms which is perfectly built for a big family<br>.You can also visit our restaurant and taste our best seller and delicacies that our restaurant offers. We have the best and savory dishes that you and your family would enjoy together with your loveones</p>
+      </div>
+
+    <div class="banner">
+        <image src="images/banner.png">
+    </div>
+
+
+    <div class="footer">
+      <p> Copyright by R&A Hotel and Restaurant 2023<br> &copy; All rights reserved </p>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-facebook"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="fab instagram"><i class="bi bi-instagram"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-twitter"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-youtube"></i></a>
+    </div>  
+
+
+
+  </body>
+</html>
