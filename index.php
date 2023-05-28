7bb9f141e1f8e7221190784963044651224a3ea3
reccalyn <reccalynloto@gmail.com>
2023-05-28 14:15:24 +0800

    php index added

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.rtl.min.css" integrity="sha384-dc2NSrAXbAkjrdm9IYrX10fQq9SDG6Vjz7nQVKdKcJl3pC+k37e7qJR5MVSCS+wR" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+
+    <title>R&A Website</title>
+  </head>
+  <body>
+
+  <!---header--->
+
+      <div class="wrapper">
+        <div class="header">
+          <div>
+            <img src="images/logo.png">
+          </div>
+          <ul>
+            <li><a href="about.php">ABOUT US</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+            <li><a href="resto.php">RESTAURANT</a></li>
+            <li><a href="hotel.php">HOTEL</a></li>
+            <li><a href="index.php" class="active">HOME</a></li>
+          </ul>
+        </div>
+      </div>
+
+
+    <!---automatic navigation--->
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  </div>
+
+  <!---image slider start--->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/slides1.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1 style="padding: 9rem; font-family: Shelldon; font-size: 4rem; text-align:center;color:skyblue;">R&A Hotel and Restaurant</h1>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/slides2.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1 style="padding: 9rem; font-family: Shelldon; font-size: 4rem; text-align:center;color:skyblue;">R&A Hotel and Restaurant</h1>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/slides3.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1 style="padding: 9rem; font-family: Shelldon; font-size: 4rem; text-align:center;color:skyblue;">R&A Hotel and Restaurant</h1>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/slides4.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1 style="padding: 9rem; font-family: Shelldon; font-size: 4rem; text-align:center;color:skyblue;">R&A Hotel and Restaurant</h1>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+    <div class="footer">
+      <p> Copyright by R&A Hotel and Restaurant 2023<br> &copy; All rights reserved </p>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-facebook"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="fab instagram"><i class="bi bi-instagram"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-twitter"></i></a>
+      <a href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"><i class="bi bi-youtube"></i></a>
+    </div>  
+    
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+   
+  </body>
+</html>
